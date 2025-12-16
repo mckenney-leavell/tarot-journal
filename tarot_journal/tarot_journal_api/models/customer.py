9e1ec3c035f73f8ecccad1659eedeefc1a97a88e
McKenney Leavell <maleavell@gmail.com>
2025-12-16 13:31:05 -0600

    Initial commit

---
+++ tarot_journal/tarot_journal_api/models/customer.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Customer(models.Model):
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.DO_NOTHING,
+    )
+    phone_number = models.CharField(max_length=15)
+    # address = models.CharField(max_length=55)
+    # likes = models.ManyToManyField("bangazonapi.Product", related_name="likes")
+
+    @property
+    def recommends(self):
+        return self.__recommends
+
+    @recommends.setter
+    def recommends(self, value):
+        self.__recommends = value
