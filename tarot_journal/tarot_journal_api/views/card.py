567db42ab959df031765ee00d0c47be9210c90ea
McKenney Leavell <maleavell@gmail.com>
2026-01-07 18:24:17 -0600

    Retrieve individual card

--- tarot_journal/tarot_journal_api/views/card.py
+++ tarot_journal/tarot_journal_api/views/card.py
@@ -45,7 +45,7 @@ class Cards(ViewSet):
                     "id": 1,
                     "url": "http://localhost:8000/cards/1",
                     "user": "http://localhost:8000/users/3"
-                    "title": "Career Spread"
+                    "title": "Career card"
                     "created_date": "2025-12-01",
                     "interpretation": "There are big changes coming your way--trust your intuition.",
                     "url": "url.example"
@@ -61,7 +61,54 @@ class Cards(ViewSet):
         except Card.DoesNotExist:
             return Response(
                 {
-                    "message": "The requested spread does not exist, or you do not have permission to access it."
+                    "message": "The requested card does not exist, or you do not have permission to access it."
+                },
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+        
+    def retrieve(self, request, pk=None):
+        """
+        @api {GET} /card/:id GET single card
+        @apiName GetCard
+        @apiGroup Cards
+
+        @apiHeader {String} Authorization Auth token
+        @apiHeaderExample {String} Authorization
+            Token 9ba45f09651c5b0c404f37a2d2572c026c146611pbkdf2_sha256$150000$fHDURJBIASpx$trZS1MWc6YiNe5EYNBap+P+zMAwpNgNbUZH/b9bgvdw=
+
+        @apiSuccess (200) {id} id Card id
+        @apiSuccess (200) {String} url Card URL
+        @apiSuccess (200) {String} name Card name
+        @apiSuccess (200) {Int} value Number/suit associated with minor arcana
+        @apiSuccess (200) {Int} element Element associated with minor arcana
+        @apiSuccess (200) {Bool} major_arcana True if card is major arcana
+        @apiSuccess (200) {String} meaning_upright Upright card meaning
+        @apiSuccess (200) {String} meaning_reverse Reverse card meaning
+        
+
+        @apiSuccessExample {json} Success
+            {
+            "name": "Page of Wands",
+            "value_id": 11,
+            "element_id": 4,
+            "major_arcana": false,
+            "meaning_upright": "Inspiration, ideas, discovery, limitless potential, free spirit",
+            "meaning_reverse": "Newly-formed ideas, redirecting energy, self-limiting beliefs, a spiritual path",
+            "url": "https://steve-p.org/cards/pix/RWSa-W-J1.png"
+            }
+        """
+        try:
+            card = Card.objects.get(pk=pk)
+            serializer = CardSerializer(card, context={"request": request})
+            return Response(serializer.data)
+        
+        except Card.DoesNotExist:
+            return Response(
+                {
+                    "message": "The requested card does not exist, or you do not have permission to access it."
                 },
                 status=status.HTTP_404_NOT_FOUND,
             )
